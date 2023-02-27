5e720ef531aa8bfae945ce6c0ef14ff308f732ea
Sam Mirosh <sammirosh@gmail.com>
2023-02-27 12:57:37 +0200

    feat: Add Profile pege_2

--- src/entities/Profile/model/slice/profileSlice.ts
+++ src/entities/Profile/model/slice/profileSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { ProfileSchema } from '../types/Profile';
+import { ProfileSchema } from 'entities/Profile';
 
 const initialState: ProfileSchema = {
   readonly: true,
